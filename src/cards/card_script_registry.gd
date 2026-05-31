beca1a70603c8ecfabea0a3b9994f2217f72befb
Jeremy <ggajy123@gmail.com>
2026-05-30 20:59:43 -0700

    feat(writing): Offering negates deck damage for an opponent Orange

--- src/cards/card_script_registry.gd
+++ src/cards/card_script_registry.gd
@@ -54,6 +54,7 @@ static func _build() -> void:
 	_register("writing", 15, CitrusSacrifice.new())
 	_register("writing", 16, CitrusSacrifice.new())
 	_register("writing", 17, PainSplit.new())
+	_register("writing", 21, Offering.new())
 
 static func get_script_for(deck: String, id: int) -> CardScript:
 	_build()
